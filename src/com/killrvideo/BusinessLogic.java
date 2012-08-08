d93156b8abb15ee24651b9f96d9173a22e453812
Patrick McFadin <pmcfadin@gmail.com>
2012-08-08 09:40:52 -0700

    Added comments

--- src/com/killrvideo/BusinessLogic.java
+++ src/com/killrvideo/BusinessLogic.java
@@ -1,6 +1,5 @@
 package com.killrvideo;
 
-
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.UUID;
@@ -41,19 +40,27 @@ public class BusinessLogic {
 	public User getUser(String username) {
 
 		// TODO Implement this method
+		/*
+		 * A simple method to get a single user based on the username key. After
+		 * you get the data from Cassandra, populate and return a User object.
+		 */
 
 		return null;
 	}
 
 	public void setVideo(Video video, Keyspace keyspace) {
-		
+
 		Mutator<UUID> mutator = HFactory.createMutator(keyspace, UUIDSerializer.get());
 
 		try {
-			mutator.addInsertion(video.getVideoId(), "videos", HFactory.createStringColumn("videoname", video.getVideoName()));
-			mutator.addInsertion(video.getVideoId(), "videos", HFactory.createStringColumn("username", video.getUsername()));
-			mutator.addInsertion(video.getVideoId(), "videos", HFactory.createStringColumn("description", video.getDescription()));
-			mutator.addInsertion(video.getVideoId(), "videos", HFactory.createStringColumn("tags", video.getDelimitedTags()));
+			mutator.addInsertion(video.getVideoId(), "videos",
+					HFactory.createStringColumn("videoname", video.getVideoName()));
+			mutator.addInsertion(video.getVideoId(), "videos",
+					HFactory.createStringColumn("username", video.getUsername()));
+			mutator.addInsertion(video.getVideoId(), "videos",
+					HFactory.createStringColumn("description", video.getDescription()));
+			mutator.addInsertion(video.getVideoId(), "videos",
+					HFactory.createStringColumn("tags", video.getDelimitedTags()));
 
 			mutator.execute();
 		} catch (HectorException he) {
@@ -92,12 +99,16 @@ public class BusinessLogic {
 
 		try {
 
-			UUIDmutator.addInsertion(video.getVideoId(), "videos", HFactory.createStringColumn("videoname", video.getVideoName()));
-			UUIDmutator.addInsertion(video.getVideoId(), "videos", HFactory.createStringColumn("username", video.getUsername()));
-			UUIDmutator.addInsertion(video.getVideoId(), "videos", HFactory.createStringColumn("description", video.getDescription()));
-			UUIDmutator.addInsertion(video.getVideoId(), "videos", HFactory.createStringColumn("tags", video.getDelimitedTags()));
+			UUIDmutator.addInsertion(video.getVideoId(), "videos",
+					HFactory.createStringColumn("videoname", video.getVideoName()));
+			UUIDmutator.addInsertion(video.getVideoId(), "videos",
+					HFactory.createStringColumn("username", video.getUsername()));
+			UUIDmutator.addInsertion(video.getVideoId(), "videos",
+					HFactory.createStringColumn("description", video.getDescription()));
+			UUIDmutator.addInsertion(video.getVideoId(), "videos",
+					HFactory.createStringColumn("tags", video.getDelimitedTags()));
 
-			for(String tag: video.getTags()){
+			for (String tag : video.getTags()) {
 				mutator.addInsertion(tag, "tag_index", HFactory.createStringColumn(video.getVideoId().toString(), ""));
 			}
 
@@ -112,7 +123,11 @@ public class BusinessLogic {
 
 	public void setVideoWithUserIndex(Video video, Keyspace keyspace) {
 		// TODO Implement this method
-
+		/*
+		 * This mthod is similar to the setVideo but with a subtle twist. When
+		 * you insert a new video, you will need to insert into the
+		 * username_video_index at the same time for username to video lookups.
+		 */
 
 	}
 
@@ -132,7 +147,21 @@ public class BusinessLogic {
 
 	public ArrayList<String> getComments(UUID videoId) {
 		// TODO Implement
-		
+		/*
+		 * Each video can have a unbounded list of comments associated with it.
+		 * This method should return all comments associated with one video.
+		 */
+
+		return null;
+	}
+
+	public ArrayList<String> getCommentsOnTimeSlice(Timestamp startTimestamp, Timestamp stopTimestamp, UUID videoId) {
+		// TODO Implement
+		/*
+		 * Each video can have a unbounded list of comments associated with it.
+		 * This method should return comments from one timestamp to another.
+		 */
+
 		return null;
 	}
 
@@ -152,8 +181,12 @@ public class BusinessLogic {
 
 	public float getRating(UUID videoId) {
 		// TODO Implement
-		
-		
+		/*
+		 * Each video has two things. a rating_count and rating_total. The
+		 * average rating is calculated by taking the total and dividing by the
+		 * count. Build the logic to get both numbers and return the average.
+		 */
+
 		return 0;
 	}
 
@@ -190,7 +223,12 @@ public class BusinessLogic {
 
 	public Timestamp getVideoLastStopEvent(UUID videoId, String username) {
 		// TODO Implement
-		
+		/*
+		 * This method will return the video timestamp of the last stop event
+		 * for a given video identified by videoid. As a hint, you will be using
+		 * a getSlice to find certain strings to narrow the search.
+		 */
+
 		return null;
 	}
 }
